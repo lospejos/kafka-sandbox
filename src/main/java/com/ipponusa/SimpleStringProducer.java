3c5d9164b88ec799714c909c38e6e5681d2ce5e2
Alexis Seigneurin <alexis@seigneurin.com>
2016-03-01 16:57:35 -0500

    kafka + spark streaming

--- src/main/java/com/ipponusa/SimpleStringProducer.java
+++ src/main/java/com/ipponusa/SimpleStringProducer.java
@@ -7,16 +7,17 @@ import java.util.Properties;
 
 public class SimpleStringProducer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
         props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         KafkaProducer<String, String> producer = new KafkaProducer<>(props);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1000; i++) {
             ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "value-" + i);
             producer.send(record);
+            Thread.sleep(250);
         }
 
         producer.close();
