403f304693845e3af20c1ff69579b2e8c267ccb9
Alexis Seigneurin <alexis@seigneurin.com>
2016-03-01 11:23:17 -0500

    initializing the project

---
+++ src/main/java/com/ipponusa/SimpleStringProducer.java
@@ -0,0 +1,24 @@
+package com.ipponusa;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+public class SimpleStringProducer {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++) {
+            ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", "value-" + i);
+            producer.send(record);
+        }
+
+        producer.close();
+    }
+}
